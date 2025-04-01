1c576c09eb8958111c31910a6614751797f591bf
deyvison iure <deyvison.iure@gmai.com>
2025-04-01 09:18:58 -0300

    coloquei o salario percentual do salario e o salario final

--- exercicio01.py
+++ exercicio01.py
@@ -1,10 +1,15 @@
 nome=input("digite o seu  nome: ")
 idade= int(input("digite a sua idade: "))
 salario=float(input("digite seu salario: "))
-
-print(f"nome: {nome}\nidade: {idade}\nsalario: R$ {salario}")
-if salario <= 1200:
-    print("----- tu é pobre -----")
-else:
-    print("----- da pra viver -----")
+percentual= int(input("digite o aumento percentual do seu salario: "))
+aumento= (salario*percentual)/100
+salario_final= salario+aumento
+print(f"olá {nome}, você tem {idade} anos, seu salario antigo é {salario}, com um acrescimo de {percentual}%\n seu novo salario é {salario_final}")
+print(f"----------------------------------------")
+print(f"> nome: {nome}")
+print(f"> idade: {idade}")
+print(f"> salario: R${salario:.2f}")
+print(f"> percentual de aumento: {percentual}%")
+print(f"> novo salario: R${salario_final:.2f}")
+print(f"----------------------------------------")
 
