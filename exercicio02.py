3149fa9c80b24d95eadf5f79f27c321bff419112
deyvison iure <deyvison.iure@gmai.com>
2025-04-01 09:48:40 -0300

    segundo exercicio

---
+++ exercicio02.py
@@ -0,0 +1,19 @@
+n1=float(input("digite o primeiro numero: "))
+escolha=str(input("escolha: "))
+n2=float(input("digite o segundo numero: "))
+
+if escolha =="+":
+    soma = n1 + n2
+    print(soma)
+elif escolha =="-":
+    sub = n1 - n2
+    print (sub)
+elif escolha =="/":
+    divisao = n2 / n1
+    print(divisao)
+elif escolha =="X":
+    mult = n1 * n2
+    print(mult)
+
+
+
