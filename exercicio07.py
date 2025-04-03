5a838403d5fca98b3f76d1eb4f2a32649cc46fb9
deyvison iure <deyvison.iure@gmai.com>
2025-04-03 09:39:15 -0300

    impar ou par

--- exercicio07.py
+++ exercicio07.py
@@ -2,12 +2,12 @@ Etanol=4.90
 gasolina=5.80
 
 litro=float(input("digite a quantidade de litros:"))
-combustivel=float(input("1-etanol(R$4.90)\n2-gasolina(R$5.80)\ndigite qual o combustivel:"))
-if combustivel==1:
+combustivel=input("E-etanol(R$4.90)\nG-gasolina(R$5.80)\ndigite qual o combustivel:")
+if combustivel=="e" or combustivel=="E":
     valor_etanol = litro * Etanol
     print(f"R${valor_etanol}")
 else:
-    if combustivel==2:
+    if combustivel=="g" or combustivel=="G":
         valor_gasolina = litro * gasolina
         print(F"R${valor_gasolina}")
     else:
---
+++ exercicio08.py
@@ -0,0 +1,5 @@
+numero=int(input("diigte um numero: "))
+if numero%2==0:
+    print("este numero é par")
+else:
+    print("este numero é impar")
