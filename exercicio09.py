d7be1513a20fd5d1dbc5e262b9e842d762cce164
deyvison iure <deyvison.iure@gmai.com>
2025-04-08 09:33:03 -0300

    1 numero 1 mes

---
+++ exercicio09.py
@@ -0,0 +1,27 @@
+numero=int(input("digite um numero de 1 a 12: "))
+if numero ==1:
+    print("janeiro")
+elif numero ==2:
+    print("feveiro")
+elif numero ==3:
+    print("março")
+elif numero ==4:
+    print("abril")
+elif numero ==5:
+    print("maio")
+elif numero ==6:
+    print("junho")
+elif numero ==7:
+    print("julho")
+elif numero ==8:
+    print("agosto")
+elif numero ==9:
+    print("setembro")
+elif numero ==10:
+    print("outubro")
+elif numero ==11:
+    print("novembro")
+elif numero ==12:
+    print("dezembro")
+else:
+    print("erro! este mês não existe")
